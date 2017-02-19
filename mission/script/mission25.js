05ef995c67830c2a93199d167addecded6e41a0c
Simen <decisiveboy20@hotmail.com.tw>
2017-02-20 02:50:57 +0800

    mission25

--- mission/script/mission25.js
+++ mission/script/mission25.js
@@ -1,12 +1,32 @@
-//實作收縮list
+//實作收縮list 開合資料夾
 (()=>{
 	function folderOpener (img) {
 		var open = '../img/openfolder.png';
 		var closed = '../img/closefolder.png';
-		var value = img.getAttribute('src').value;
+		var value = img.getAttribute('src');
 		value = value == open ? closed : open;
+		img.setAttribute('src' , value)
+	}
+	function listHidden (ul){
+		var hidden = 'is-hidden';
+		var showUp = 'show up';
+		ul.className = ul.className == hidden ? showUp : hidden;
 	}
 	window.onload = () => {
-		
+		var img = document.getElementsByClassName('listImg');
+		var ul = document.getElementsByClassName('is-hidden');
+		var span = document.getElementsByClassName('subtitle')
+		for(var i = 0; i < img.length; i++){
+			((img , ul , span) => {
+				img.addEventListener('click',() => {
+					folderOpener(img);
+					listHidden(ul);
+				} );
+				span.addEventListener('click',() => {
+					folderOpener(img);
+					listHidden(ul);
+				} );
+			})(img[i] , ul[i] ,span[i]);
+		}
 	};
 })();
