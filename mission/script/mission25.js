f9591601f8c669e68d977feaac874375e387c1ed
Simen <decisiveboy20@hotmail.com.tw>
2017-02-21 00:23:52 +0800

    mission25

--- mission/script/mission25.js
+++ mission/script/mission25.js
@@ -1,5 +1,5 @@
-//實作收縮list 開合資料夾
 (()=>{
+	var searchList = [];
 	function folderOpener (img) {
 		var open = '../img/openfolder.png';
 		var closed = '../img/closefolder.png';
@@ -9,24 +9,93 @@
 	}
 	function listHidden (ul){
 		var hidden = 'is-hidden';
-		var showUp = 'show up';
+		var showUp = 'show-up';
 		ul.className = ul.className == hidden ? showUp : hidden;
 	}
-	window.onload = () => {
-		var img = document.getElementsByClassName('listImg');
-		var ul = document.getElementsByClassName('is-hidden');
-		var span = document.getElementsByClassName('subtitle')
-		for(var i = 0; i < img.length; i++){
-			((img , ul , span) => {
-				img.addEventListener('click',() => {
-					folderOpener(img);
-					listHidden(ul);
-				} );
-				span.addEventListener('click',() => {
-					folderOpener(img);
-					listHidden(ul);
-				} );
-			})(img[i] , ul[i] ,span[i]);
+	function addElement (parent , childValue) {
+		var div = document.createElement('div');
+		div.className = 'classBlock';
+		var img = document.createElement('img');
+		img.className = 'listImg';
+		img.setAttribute('src','../img/closefolder.png');
+		var span = document.createElement('span');
+		span.className = 'subtitle';
+		span.innerHTML = childValue;
+		var divList = document.createElement('div');
+		divList.className = 'is-hidden';
+		div.appendChild(img);
+		div.appendChild(span);
+		div.appendChild(divList);
+		parent.appendChild(div);
+	}
+	function deleteElements (node) {
+		node.parentNode.removeChild(node);
+	}
+	function searchinit () {
+		searchList = [];
+	}
+	function searchNode (troot , value) {
+		if (troot && troot.className == 'subtitle') {
+			if(troot.innerHTML == value )
+				searchList.push(troot);
 		}
-	};
+		for(var i = 0; i < troot.children.length ; i++){
+				searchNode(troot.children[i] , value);
+			}
+		return searchList;
+	}
+	function inputClear (input) {
+		input.value = "";
+	}
+	function searchEffect (nodes) { 
+		for (var i = 0; i < nodes.length; i++) {
+			console.log(nodes);
+			var color = 100;
+			var timer = setInterval(() => {
+				var rgb = color.toString(16) + color.toString(16) + color.toString(16);
+				toUpperCase(rgb);
+				node[i].style.border = '2px solid' + ' #' + rgb;
+				color++;
+				if (color == 255) {
+					clearInterval(timer);
+				}
+			} , '100');
+		}
+	}
+	function init() {
+		window.onload = () => {
+			var input = document.getElementsByTagName('input');
+			var button = document.getElementsByTagName('button');
+			var troot = document.getElementsByClassName('main')[0];
+			troot.addEventListener('click',(event) => {
+				var element = event.target;
+				if (element.className == 'listImg' || element.className == 'subtitle') {
+					folderOpener(element = element.className == 'listImg' ? element : element.previousElementSibling);
+					listHidden(ul = element.className == 'listImg' ? element.nextElementSibling.nextElementSibling : element.nextElementSibling);
+				}
+			});
+			button[0].addEventListener('click' , ()=>{
+					var value = input[0].value ; 
+					if(!value){
+						alert('請輸入內容');
+						return ;
+					}
+					searchinit();
+					var results = searchNode(troot , value);
+					for (var i = 0; i < results.length; i++) {
+						searchEffect(results[i]);
+						var parent = results[i].parentElement.parentElement;
+						var img = parent.previousElementSibling.previousElementSibling;
+						if(parent.className == 'is-hidden'){
+							folderOpener(img);
+							listHidden(parent);
+						}
+					}	
+			});
+			button[1].addEventListener('click' , ()=>{
+				inputClear(input[0]);
+			});
+		};
+	}
+	init();
 })();
