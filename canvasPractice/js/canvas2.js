f413c976d3146cd9a5492abf65c03138ec0f8309
Simen <decisiveboy20@hotmail.com.tw>
2017-02-28 22:03:32 +0800

    canvas2practice

--- canvasPractice/js/canvas2.js
+++ canvasPractice/js/canvas2.js
@@ -1,7 +1,9 @@
 function initCanvas () {
-	var ctx = document.getElementById('canvas').getContext('2d');
+	var ctx = document.getElementById('my_canvas').getContext('2d');
 	var cW = ctx.canvas.width , cH = ctx.canvas.height;
 	var x = 0 , y = 0;
+	var bg = new Image();
+	bg.src = "images/canvas2Background.bmp";
 
 	function RectObj (x,y,w,h){
 		this.x = x , this.y = y , this.w = w ,this.h = h;
@@ -10,12 +12,24 @@ function initCanvas () {
 		ctx.fillStyle = clr;
 		ctx.fillRect(rx, ry, rw, rh);
 	};
+
+	function background () {
+		this.x = 0 , this.y = 0;
+		this.rander = () => {
+			ctx.drawImage(bg , this.x-- , 0);
+			if(this.x <= -499)
+				this.x = 0;
+		};
+	}
 	var rect1 = new RectObj(0,0,50,50);
 	var rect2 = new RectObj(0,20,50,50);
+	var background = new background(); 
+
 
 	function animate (argument) {
 		ctx.save();
 		ctx.clearRect(0,0,cW,cH);
+		background.rander();
 		rect1.rander(ctx , rect1.x , rect1.y ,rect1.w , rect1.h , 'red');
 		rect2.rander(ctx , rect2.x , rect2.y ,rect2.w , rect2.h , 'blue');
 		ctx.restore();
