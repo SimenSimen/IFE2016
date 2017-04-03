a6716d92d3ce5ecbd5c05cdb8907538bf859ab15
Simen <decisiveboy20@hotmail.com.tw>
2017-04-03 20:10:34 +0800

    mission39

--- mission/script/mission39_1.js
+++ mission/script/mission39_1.js
@@ -1,9 +1,23 @@
 (()=>{
 	function init () {
 		var title = c_('title')[0];
-		window.addEventListener('scroll' , ()=>{
-			console.log(title.scrollTop);
-		});
+		var table = c_('tg')[0];
+		function fixtop (e) {
+			var height = table.offsetHeight ;
+			var left = table.offsetLeft ;
+			if(e.target.scrollingElement.scrollTop > 50 && e.target.scrollingElement.scrollTop < height){
+				title.style.position = 'fixed';
+				title.style.top = 0;
+				title.style.left = left  -1 + 'px';
+			}
+			else if (e.target.scrollingElement.scrollTop > height) {
+				title.removeAttribute('style');
+			}
+			else if (e.target.scrollingElement.scrollTop < 30){
+				title.removeAttribute('style');
+			}
+		}
+		window.addEventListener('scroll' , fixtop);
 	}
 	window.addEventListener('load',init);
 })();
